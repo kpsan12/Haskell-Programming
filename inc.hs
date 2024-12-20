eb8a4c79d4712c544c486c68676fba296fe8e340
kpsan12 <137251415+kpsan12@users.noreply.github.com>
2024-12-20 14:23:37 +0530

    Delete inc.hs

--- inc.hs
+++
@@ -1,5 +0,0 @@
--- Function to increment each element in a list by 1
-incrementEach :: [Int] -> [Int]
-incrementEach [] = []  -- Base case: if the list is empty, return an empty list
-incrementEach (x:xs) = (x + 1) : incrementEach xs  -- Increment the first element and process the rest recursively
-

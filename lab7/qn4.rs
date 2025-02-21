3bcebff49f757e5809b2cc0bab37ffb81032ea4a
kpsan12 <137251415+kpsan12@users.noreply.github.com>
2025-02-21 14:39:19 +0530

    Update qn4.rs

--- lab7/qn4.rs
+++ lab7/qn4.rs
@@ -1,1 +1,13 @@
+use text_io::scan;
+fn main() {
+    println!("Enter a number:");
+    let number: i32;
+    scan!("{}", number);
+
+    if number %2 ==0 {
+        println!("The number is even.");
+    } else {
+        println!("The number is odd.");
+    }
+}
 

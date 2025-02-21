2dbe803bf718168ec1325bdde68d66a4cb943eaa
kpsan12 <137251415+kpsan12@users.noreply.github.com>
2025-02-21 13:40:03 +0530

    Create qn1.rs

---
+++ lab7/qn1.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let int_var: i32 = 42;
+    let float_var: f64 = 3.14;
+    let bool_var: bool = true;
+    let char_var: char = 'A';
+    println!("Integer: {}", int_var);
+    println!("Float: {}", float_var);
+    println!("Boolean: {}", bool_var);
+    println!("Character: {}", char_var);
+}
